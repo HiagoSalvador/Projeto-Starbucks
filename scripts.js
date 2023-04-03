194126fcd04b8d3593bab75dc8524de30ec48968
HiagoSalvador <hiago.salvador@hotmail.com>
2023-04-03 15:31:29 -0300

    Projeto-Starbucks

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let image= document.querySelector(".starbucks");
+let circle= document.querySelector(".circle")
+ 
+
+ function changeimage(address) {
+     image.src = address
+     
+ }
+
+ function Changethecolor(color){
+     circle.style.background= color
+
+ }
